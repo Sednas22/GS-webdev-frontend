1f9ba6d0a41339151216c6a8104deff25e3e59d9
Cainã Sandes <caina.sb2018@gmail.com>
2025-11-13 09:48:56 -0300

    fiz o css quase todo, preparei dados.js

---
+++ src/js/dados.js
@@ -0,0 +1,13 @@
+var perfis = [
+	{
+		nome: "Ana Clara Oliveira Porto",
+		cargo: "Psicóloga",
+		foto: "src/assets/imgs/Ana.png",
+		skills: ["Resolução de problemas, aprendizado contínuo, pensamento crítico."],
+		formacao: "Psicologia (Centro Universitário Braz Cubas)",
+		experiencia: "Ministração de palestras",
+		soft: ["Escuta ativa, comunicação"],
+		hobbies: ["Leitura"],
+		ods: "ODS4"
+	}
+]
